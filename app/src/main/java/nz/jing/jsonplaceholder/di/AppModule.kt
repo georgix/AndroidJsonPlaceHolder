224febd8c4e93a8f929ff6b7dcd89e6fdd1ee0a1
Jing Zhang <zhangjing.tju@gmail.com>
2022-04-18 00:28:40 +1200

    Add Retrofit2/Moshi

--- app/src/main/java/nz/jing/jsonplaceholder/di/AppModule.kt
+++ app/src/main/java/nz/jing/jsonplaceholder/di/AppModule.kt
@@ -1,10 +1,27 @@
 package nz.jing.jsonplaceholder.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: MoshiConverterFactory): Retrofit {
+        return Retrofit.Builder().baseUrl("https://g.cn")
+            .addConverterFactory(moshi)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): MoshiConverterFactory {
+        return MoshiConverterFactory.create()
+    }
 }
